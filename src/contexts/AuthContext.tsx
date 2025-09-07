00ed252ac3e2fbc23d08f8148b488d1b281059d0
Hyper1c <salazarandres1111z@gmail.com>
2025-09-07 16:34:19 -0500

    Updated AuthContext.tsx

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -96,7 +96,7 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
 
   return (
     <AuthContext.Provider value={value}>
-      {!loading && children}
+      {children}
     </AuthContext.Provider>
   );
 };
